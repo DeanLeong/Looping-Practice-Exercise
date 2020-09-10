06b342a0328470d4bf8699f41c8912dfdaa7e61d
Steven VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-09-10 13:54:19 -0600

    lab solution

--- exercises.js
+++ exercises.js
@@ -1,10 +1,76 @@
 //Exercise 1
 
+// let temp = 100
+
+// if (temp <= 32) {
+//   console.log("Watch out for ice.")
+// } else if (temp >= 33 && temp <= 55) {
+//   console.log("Better put on a jacket")
+// } else if (temp >= 56 && temp <= 70) {
+//   console.log("Such nice weather we're having")
+// } else if (temp >= 71 && temp <= 99) {
+//   console.log("Summer is here")
+// } else if (temp === 212 || temp === 444) {
+//   console.log("Where are you exactly?")
+// } else {
+//   console.log("Global Warming!")
+// }
+
 //Exercise 2
 
+let month = new Date()
+month = month.getMonth()
+console.log(month)
+month++
+// num = 0
+// num = num + 1
+// num += 1
+// num++
+console.log(month)
+
+let isSeptember = (month === 9) ? `It's September`: `I don't know what month it is?`
+console.log(isSeptember)
 //Exercise 3
+
+// let foodsArray = ['almonds', 'potato chips', 'ice cream', 'filet mignon'];
+
+// for (let i = 0; i < foodsArray.length; i++) {
+//   // console.log(foodsArray[i])
+//   switch (foodsArray[i]) {
+//     case 'almonds':
+//       console.log(`I love ${foodsArray[i]}`)
+//       break;
+//     case 'potato chips':
+//       console.log(`${foodsArray[i]}: moment on the lips, lifetime on the hips!`)
+//       break;
+//     case 'ice cream':
+//       console.log('Something sweet, ok!')
+//       break;
+//     case 'filet mignon':
+//       console.log(`Eating like royalty tonight, having ${foodsArray[i]}.`)
+//       break;
+//     default:
+//       console.log( 'I don\'t know what to eat.')
+//   }
+// }
 
 //Exercise 4
 
+// let accountBalance = 0
+// let message = ''
+// while (accountBalance <= 100) {
+//   message = `My checking account balance is $${accountBalance}.`
+//   console.log(message)
+//   accountBalance++
+// }
+
+// let steve = "Outside Steve"
+// console.log(steve)
+// {
+//   let steve = 'Inside Steve'
+//   console.log(steve)
+// }
+
+// console.log(steve)
 
 //Bonus
